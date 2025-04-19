3531832c9b326342f3215d9709b62706188762ed
nemethkoppany <koppany344@gmail.com>
2025-04-19 20:53:43 +0200

    tablazat letrehozasa oop

--- oop/area.js
+++ oop/area.js
@@ -1,4 +1,17 @@
 class Area{
+
+    /**
+     * @type {HTMLDivElement}
+     */
+    #div
+
+    /**
+     * @returns {HTMLDivElement}
+     */
+    get div(){
+        return this.#div;
+    }
+
     /**
      * 
      * @param {string} nameOfTheClass 
@@ -11,8 +24,39 @@ class Area{
             document.body.appendChild(containerDiv);
         }
 
-        const div = document.createElement("div");
-        div.className = nameOfTheClass;
-        containerDiv.appendChild(div);
+        this.#div = document.createElement("div");
+        this.#div.className = nameOfTheClass;
+        containerDiv.appendChild(this.#div);
     }
+}
+
+class Table extends Area{
+    /**
+     * 
+     * @param {string} nameOfTheClass 
+     */
+    constructor(nameOfTheClass){
+        super(nameOfTheClass)
+
+        const table = document.createElement('table');
+        this.div.appendChild(table);
+ 
+
+        const thead = document.createElement('thead');
+        table.appendChild(thead);
+
+        const tr = document.createElement('tr');
+        thead.appendChild(tr);
+
+        const theadCells = ['Szerző', 'műfaj', 'cím'];
+        for(const content of theadCells){
+            const th = document.createElement('th');
+            th.innerText = content;
+            tr.appendChild(th);
+        }
+        const tbody = document.createElement('tbody');
+        table.appendChild(tbody);
+    }
+
+    
 }
