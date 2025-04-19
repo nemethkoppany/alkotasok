db5ecba700666b8425f0f185bcd890999cf1a6e2
nemethkoppany <koppany344@gmail.com>
2025-04-19 20:46:18 +0200

    tablazat hozzadasa sima

--- main.js
+++ main.js
@@ -13,6 +13,26 @@ const containerDiv = divmaker("container");
 document.body.appendChild(containerDiv);
 const tableDiv = divmaker("table");
 
+const tableRegular = document.createElement("table");
+tableDiv.appendChild(tableRegular);
+
+const thead = document.createElement("thead");
+tableRegular.appendChild(thead);
+
+const tr = document.createElement("tr");
+thead.appendChild(tr);
+
+const headercells = ["Szerző","Műfaj", "Cím"];
+for(const header of headercells){
+    const th = document.createElement("th");
+    th.innerHTML = header;
+    tr.appendChild(th);
+}
+
+const tbody = document.createElement("tbody");
+tableRegular.appendChild(tbody);
+
+
 const formDiv = divmaker("form");
 
 containerDiv.appendChild(tableDiv);
